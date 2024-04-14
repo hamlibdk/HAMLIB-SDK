996143086ec5e7eba11cfb2af806be88c1ba1129
hamlibdk <hamlibdk@outlook.com>
2024-04-14 15:13:38 +1000

    Documentation Updates - Including Qt Notes

---
+++ docs/ver4/nav4.php
@@ -0,0 +1,14 @@
+<?php
+echo ' <table width="100%" border="0"> -
+	<tr> -
+		<td width="14.2%" align="center"><a href="../ver4/DG4.html">Installation</a></td> - 
+		<td width="14.3%" align="center"><a href="../ver4/DG4-Build-x64-JT-ware.html">Build x64</a></td> -
+		<td width="14.3%" align="center"><a href="../ver4/DO4.html">Developers</a></td> -
+		<td width="14.3%" align="center"><a href="../ver4/AN4.html">Notes</a></td> - 
+		<td width="14.3%" align="center"><a href="../ver4/QU4.html">Questions</a></td> -
+		<td width="14.3%" align="center"><a href="https://sourceforge.net/projects/hamlib-sdk/">Sourceforge</a></td> - 
+		<td width="14.2%" align="center"><a href="https://sourceforge.net/projects/hamlib-sdk/files/Windows/JTSDK-4.0-Stream/">Download</a</td> -
+	</tr> -
+</table> -
+<hr/> ';
+?>
